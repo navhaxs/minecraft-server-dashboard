47a75fed396b571f5909030119eb8feef80f755b
navhaxs <navhaxs@hotmail.com>
2013-10-28 23:51:56 +1100

    downloader fixes (continued)

---
+++ MinecraftServerDashboard/ConfigPages/Pages/Dialogs/DownloadWindow.xaml.vb
@@ -0,0 +1,23 @@
+﻿Public Class DownloadWindow
+
+    Sub New(UpdaterEngine As UpdateEngine)
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        m_UpdaterEngine = UpdaterEngine
+    End Sub
+
+    Public m_UpdaterEngine As UpdateEngine
+
+    Private Sub Button1_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles Button1.Click
+        CancelDownload()
+    End Sub
+
+    Private Sub CancelDownload()
+        Me.Close()
+        m_UpdaterEngine.client.CancelAsync()
+        Dim n As New MessageWindow(MyMainWindow, "", "The download has been cancelled.", "Cancelled")
+    End Sub
+End Class
