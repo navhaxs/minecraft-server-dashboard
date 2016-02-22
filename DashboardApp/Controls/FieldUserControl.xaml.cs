78a9f776c7dd03980c43afc1b4ac2e2f5c88876a
Jeremy Wong <navhaxs@hotmail.com>
2016-02-22 22:37:14 +1100

    Add untracked files

---
+++ DashboardApp/Controls/FieldUserControl.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DashboardApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for FieldUserControl.xaml
+    /// </summary>
+    public partial class FieldUserControl : UserControl
+    {
+        #region Label DP
+
+        /// <summary>
+        /// Gets or sets the Label which is displayed next to the field
+        /// </summary>
+        public String Label
+        {
+            get { return (String)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        /// <summary>
+        /// Identified the Label dependency property
+        /// </summary>
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string),
+              typeof(FieldUserControl), new PropertyMetadata(""));
+
+        #endregion
+
+        #region Label DP
+
+        /// <summary>
+        /// Gets or sets the Value which is being displayed
+        /// </summary>
+        public object Value
+        {
+            get { return (object)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        /// <summary>
+        /// Identified the Label dependency property
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(object),
+              typeof(FieldUserControl), new PropertyMetadata(null));
+
+        #endregion
+
+
+        public FieldUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DashboardApp/Controls/btnHelp.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DashboardApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for btnHelp.xaml
+    /// </summary>
+    public partial class btnHelp : Button
+    {
+        public btnHelp()
+        {
+            InitializeComponent();
+        }
+
+        private void thisGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
